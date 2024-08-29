70c2cd889e5b736766b97da534d15d9bc5c711a0
IGR2020 <159914009+IGR2020@users.noreply.github.com>
2024-08-29 15:13:25 +0530

    Player! (41 Lines)

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
 import pygame as pg, random, os
-pg.mixer.init()
-sounds, assets, objects, item_size, run, window, slime_blocks, clock, slime_image, slime_sound = [pg.mixer.Sound("assets/sounds/break1.ogg"), pg.mixer.Sound("assets/sounds/place1.ogg")], {}, [], 32, True, pg.display.set_mode((450, 750), pg.RESIZABLE), [], pg.time.Clock(), pg.transform.scale2x(pg.image.load("assets/other/Slime Block.png")), pg.mixer.Sound("assets/sounds/slime.ogg")
-window_width, window_height, = window.get_size()
-pg.display.set_caption("Internet Game!")
-pg.font.init()
+class Player(pg.Rect):
+    def __init__(self, x: int, y: int, move_left, move_right, idle): _, self.leftImage, self.rightImage, self.idleImage, self.x_vel, self.y_vel, self.image = super().__init__(x, y, item_size, item_size*2), move_left, move_right, idle, 0, 0, idle
+    def display(self, window): window.blit(player_assets[self.image], self)
+    def script(self):
+        keys, self.x_vel = pg.key.get_pressed(), 0
+        if keys[pg.K_a]: self.x_vel -= 2
+        if keys[pg.K_d]: self.x_vel += 2
+        self.x, self.y = self.x + self.x_vel, self.y + self.y_vel
+        if self.x_vel > 0: self.image = self.rightImage
+        elif self.x_vel < 0: self.image = self.leftImage
+        else: self.image = self.idleImage
+window, _, _ = pg.display.set_mode((450, 750), pg.RESIZABLE), pg.mixer.init(), pg.display.set_caption("Internet Game!")
+sounds, assets, objects, item_size, run, slime_blocks, clock, slime_image, slime_sound, player_assets, oof_sound = [pg.mixer.Sound("assets/sounds/break1.ogg"), pg.mixer.Sound("assets/sounds/place1.ogg")], {}, [], 32, True, [], pg.time.Clock(), pg.transform.scale2x(pg.image.load("assets/other/Slime Block.png")), pg.mixer.Sound("assets/sounds/slime.ogg"), {}, pg.mixer.Sound("assets/sounds/oof.mp3")
 for file in os.listdir("assets/items"): assets[file.replace(".png", "")] = pg.transform.scale2x(pg.image.load(f"assets/items/{file}"))
-def blit_text(win, text, pos, colour=(0, 0, 0), size=30, font="arialblack"):
-    text = str(text)
-    x, y = pos
-    font_style = pg.font.SysFont(font, size)
-    text_surface = font_style.render(text, True, colour)
-    win.blit(text_surface, (x, y))
+for file in os.listdir("assets/player"): player_assets[file.replace(".png", "")] = pg.transform.scale2x(pg.image.load(f"assets/player/{file}"))
+window_width, window_height, player = window.get_width(), window.get_height(), Player(200, 750-item_size*2, "Stone Left", "Stone Right", "Stone Idle")
 class Item(pg.Rect):
     def __init__(self, x, y, name) -> None: self.name, self.y_vel, _ = name, 0, super().__init__(x, y, item_size, item_size)
 while run:
@@ -21,20 +25,17 @@ while run:
             if event.button != 3: 
                 objects.append(Item(x, y, random.choice(list(assets.keys()))))
                 random.choice(sounds).play()
-            if event.button == 3: 
-                slime_blocks.append(Item((x // item_size)*item_size
-                (y// item_size)*item_size, None))
-                slime_sound.play()
+            if event.button == 3: slime_blocks.append(Item((x // item_size)*item_size, (y// item_size)*item_size, None)), slime_sound.play()
         if event.type == pg.VIDEORESIZE: window_width, window_height = event.dict["size"]
-    window.fill((30, 30, 30)),  clock.tick(60)
+    window.fill((30, 30, 30)),  clock.tick(60), player.display(window), player.script()
     for obj in objects:
         if obj.bottom < window_height: obj.y_vel += 0.2 
         else: obj.y_vel = 0
+        if player.colliderect(obj): oof_sound.play(), objects.remove(obj)
         obj.y , _ = obj.y + obj.y_vel, window.blit(assets[obj.name], obj)
     for slime in slime_blocks: 
         window.blit(slime_image, slime.topleft)
-        for obj in objects:
-            if slime.colliderect(obj):
-                obj.y_vel = -obj.y_vel * 0.7
+        for obj in objects: 
+            if slime.colliderect(obj): obj.y_vel = -obj.y_vel * 0.7
     pg.display.update()
 pg.quit()
